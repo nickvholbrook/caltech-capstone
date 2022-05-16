a43240c0da2338410bb0ef6e7167755f8c4cb7da
Nick Holbrook <nick.holbrook@gmail.com>
2022-05-16 15:04:00 +0100

    Commented out node1 while testing AWS access

--- main.tf
+++ main.tf
@@ -10,11 +10,11 @@ terraform {
 provider "aws" {}
 
 resource "aws_instance" "controlplane" {
-  ami           = "ami-0022f774911c1d690"
+  ami           = "ami-0d729d2846a86a9e7"
   instance_type = "t2.micro"
 }
 
-resource "aws_instance" "node1" {
-  ami           = "ami-0022f774911c1d690"
-  instance_type = "t2.micro"
-}
+# resource "aws_instance" "node1" {
+#   ami           = "ami-0d729d2846a86a9e7"
+#   instance_type = "t2.micro"
+# }
