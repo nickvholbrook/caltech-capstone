02b039d25ffaf8d5291a31c71d1205963a88823b
Nick Holbrook <nick.holbrook@gmail.com>
2022-05-16 16:13:13 +0100

    Added user-data and keypair

--- main.tf
+++ main.tf
@@ -12,9 +12,42 @@ provider "aws" {}
 resource "aws_instance" "controlplane" {
   ami           = "ami-0d729d2846a86a9e7"
   instance_type = "t2.micro"
+  key_name = k8s-keypair
+  tags = {
+    name = "controlplane"
+  }
+  user_data = <<EOF
+  apt-get update -y
+  sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+  echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+  apt-get update
+  apt install -y kubeadm=1.20.5-00 kubelet=1.20.5-00 kubectl docker.io
+
+  EOF
+
 }
 
 # resource "aws_instance" "node1" {
 #   ami           = "ami-0d729d2846a86a9e7"
 #   instance_type = "t2.micro"
+
+#     tags = {
+#     name = "node1"
+#   }
 # }
+
+
+
+# resource "aws_lb" "test" {
+#   name               = "k8s-loadbalancer"
+#   internal           = false
+#   load_balancer_type = "network"
+#   subnets            = [for subnet in aws_subnet.public : subnet.id]
+
+#   enable_deletion_protection = true
+
+#   tags = {
+#     Environment = "production"
+#   }
+# }
+
