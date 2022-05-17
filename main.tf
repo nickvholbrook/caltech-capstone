548bc41b69aad6abeb09f6146100c0d087132b6f
Nick Holbrook <nick.holbrook@gmail.com>
2022-05-17 16:41:46 +0100

    Change provider version

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 3.55.0"
     }
   }
 }
@@ -11,12 +11,22 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_instance" "controlplane" {
-  ami           = "ami-0a244485e2e4ffd03"
+data "aws_ami_ids" "ubuntu" {
+  owners = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/ubuntu-*-*-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "controlplane1" {
+  ami = data.aws_ami.ubuntu.id
+  #ami           = "ami-0a244485e2e4ffd03"
   instance_type = "t2.medium"
   key_name      = "k8s-keypair"
   tags = {
-    name = "controlplane"
+    name = "controlplane1"
   }
 
   user_data =<<EOF
@@ -32,15 +42,28 @@ EOF
 
 }
 
-# resource "aws_instance" "node1" {
-#   ami           = "ami-0a244485e2e4ffd03"
-#   instance_type = "t2.micro"
+resource "aws_instance" "controlplane2" {
+  ami = data.aws_ami.ubuntu.id
+  #ami           = "ami-0a244485e2e4ffd03"
 
-#     tags = {
-#     name = "node1"
-#   }
-# }
+  instance_type = "t2.medium"
+  key_name      = "k8s-keypair"
+  tags = {
+    name = "controlplane2"
+  }
 
+  user_data =<<EOF
+#!/bin/bash
+sudo hostname "controlplane" 
+sudo apt-get update -y
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+sudo echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt install -y kubeadm=1.20.5-00 kubelet=1.20.5-00 kubectl docker.io
+
+EOF
+
+}
 
 
 # resource "aws_lb" "test" {
