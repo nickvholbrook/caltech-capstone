e16f4d08ead04208e8e69266e7c7c54b2dc53d8e
Nick Holbrook <nick.holbrook@gmail.com>
2022-05-19 21:00:25 +0100

    Appended ETCD snapshot details to commands.sh

--- commands.sh
+++ commands.sh
@@ -2,3 +2,10 @@
 # Install Calico CNI
 kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 
+# Snapshot the ETCD database
+ETCDCTL_API=3 etcdctl --endpoints $ENDPOINT snapshot save snapshotdb
+
+#Verify the snapshot:
+
+ETCDCTL_API=3 etcdctl --write-out=table snapshot status snapshotdb
+
